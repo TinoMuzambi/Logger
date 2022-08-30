72de7d7e3898aa66377d147197a24b5fce769374
Tinotenda Muzambi <tinomuzambi@gmail.com>
2022-08-30 12:25:59 +0000

    Adds cors.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors")
 require("dotenv").config();
 const feasibilityRoutes = require("./routes/feasibilityRoutes");
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
 const PORT = process.env.PORT || 5000;
 
