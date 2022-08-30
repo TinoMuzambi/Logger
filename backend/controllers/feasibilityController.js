835b30485c524a285fc756eea79dee81f54ba3ad
Tino Muzambi <tinomuzambi@gmail.com>
2022-08-30 10:12:23 +0200

    Adds db connect.

--- backend/controllers/feasibilityController.js
+++ backend/controllers/feasibilityController.js
@@ -15,80 +15,95 @@ const options = {
 		});
 	},
 
-    show: async function (req, res) {
-        const id = req.params.id
+	show: async function (req, res) {
+		await dbConnect();
+		const id = req.params.id;
 
-        FeasibilityModel.findOne({_id: id}, async (err, feasibility) => {
-            if (err) {
-                return res.status(500).json({err})
-            }
+		FeasibilityModel.findOne({ _id: id }, async (err, feasibility) => {
+			if (err) {
+				return res.status(500).json({ err });
+			}
 
-            if (!tutorInfo) {
-                return res.status(404).json({
-                    message: "No such feasibility"
-                })
-            }
+			if (!feasibility) {
+				return res.status(404).json({
+					message: "No such feasibility",
+				});
+			}
 
-            return res.status(200).json({feasibility})
-        })
-    },
+			return res.status(200).json({ feasibility });
+		});
+	},
 
-    create: async function (req, res) {
-        const feasibility = new FeasibilityModel(req.body)
+	create: async function (req, res) {
+		await dbConnect();
+		const feasibility = new FeasibilityModel(req.body);
 
-        feasibility.save((err, feasibility) => {
-            if (err) {
-                return res.status(500).json({err})
-            }
+		feasibility.save((err, feasibility) => {
+			if (err) {
+				return res.status(500).json({ err });
+			}
 
-            return res.status(201).json(feasibility)
-        })
-    },
+			return res.status(201).json(feasibility);
+		});
+	},
 
-    update: async function (req, res) {
-        const id = req.params.id;
-        console.log(id);
+	update: async function (req, res) {
+		await dbConnect();
+		const id = req.params.id;
 
-        FeasibilityModel.findOne({_id: id}, (err, feasibility) => {
-            if (err) {
-                return res.status(500).json({err, message: "Error finding feasibility"})
-            }
+		FeasibilityModel.findOne({ _id: id }, (err, feasibility) => {
+			if (err) {
+				return res
+					.status(500)
+					.json({ err, message: "Error finding feasibility" });
+			}
 
-            if (!feasibility) {
-                return res.status(404).json({
-                    message: "No such feasibility"
-                })
-            }
+			if (!feasibility) {
+				return res.status(404).json({
+					message: "No such feasibility",
+				});
+			}
 
-            console.log(req.body);
+			feasibility.name = req.body.name ? req.body.name : feasibility.name;
+			feasibility.numberOfSites = req.body.numberOfSites
+				? req.body.numberOfSites
+				: feasibility.numberOfSites;
+			feasibility.product = req.body.product
+				? req.body.product
+				: feasibility.product;
+			feasibility.dateReceived = req.body.dateReceived
+				? req.body.dateReceived
+				: feasibility.dateReceived;
+			feasibility.dateCompleted = req.body.dateCompleted
+				? req.body.dateCompleted
+				: feasibility.dateCompleted
+				? feasibility.dateCompleted
+				: null;
 
-            feasibility.name = req.body.name ? req.body.name : feasibility.name
-            feasibility.numberOfSites = req.body.numberOfSites ? req.body.numberOfSites : feasibility.numberOfSites
-            feasibility.product = req.body.product ? req.body.product : feasibility.product
-            feasibility.dateReceived = req.body.dateReceived ? req.body.dateReceived : feasibility.dateReceived
-            feasibility.dateCompleted = req.body.dateCompleted ? req.body.dateCompleted : feasibility.dateCompleted ? feasibility.dateCompleted : null
+			feasibility.save((err, updatedFeasibility) => {
+				if (err) {
+					return res
+						.status(500)
+						.json({ err, message: "Error saving feasibility" });
+				}
 
-            feasibility.save((err, updatedFeasibility) => {
-                if (err) {
-                    return res.status(500).json({err, message: "Error saving feasibility"})
-                }
+				return res.status(201).json(updatedFeasibility);
+			});
+		});
+	},
 
-                return res.status(201).json(updatedFeasibility)
-            })
-        })
-    },
+	remove: async function (req, res) {
+		await dbConnect();
+		const id = req.params.id;
 
-    remove: async function (req, res) {
-        const id = req.params.id
+		FeasibilityModel.findByIdAndRemove(id, (err, feasibility) => {
+			if (err) {
+				return res.status(500).json({ err });
+			}
 
-        FeasibilityModel.findByIdAndRemove(id, (err, feasibility) => {
-            if (err) {
-                return res.status(500).json({err})
-            }
-
-            return res.status(204).json({feasibility})
-        })
-    }
+			return res.status(204).json({ feasibility });
+		});
+	},
 };
 
 module.exports = options;
